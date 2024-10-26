94fb02af6a383bd23ffb6ea9ae2490a5733fdbf8
Nishreyta-Lamsal <nishreytal@gmail.com>
2024-10-26 16:45:30 +0545

    First Commit

---
+++ appoinments/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Appoinments from "./Appoinments";
+import Header from "./Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Appoinments />
+    </>
+  );
+};
+
+export default App;
